cb9ec367c4bcc8a1b5fbf75b5ed9ab54383a0d8a
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2025-01-04 18:57:05 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to add additional functionality if needed
+document.getElementById('video-player').addEventListener('play', function() {
+    console.log('Video is playing!');
+});
+
+document.getElementById('video-player').addEventListener('pause', function() {
+    console.log('Video is paused!');
+});
